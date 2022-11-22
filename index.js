a55861b0549f5511ee84fe14f097cfeb02da6b6b
FranklinKipchumba <sitieneifranklin@gmail.com>
2022-11-22 15:52:11 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-function introduction(name = "Aki"){
-    console.log('Hi, my name is ${name}.');   
+function introduction(name) {
+    console.log(`Hi,my name is ${name}.`);
+    return`Hi, my name is ${name}.`;
+}
+introduction("Aki");
+
+function introductionWithLanguage(name, language) {
+    console.log(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+    return`Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+introductionWithLanguage("Aki", "Ember.js");
+introductionWithLanguage("Samip", "React");
+
+function introductionWithLanguageOptional(name, language="JavaScript") {
+    console.log(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
 }
 
-function introduction(name = "Samip"){
-    console.log('Hi, my name is ${name}.');   
+introductionWithLanguageOptional("Gracie");
+
+function introductionWithLanguageOptional(name, language="JavaScript") {
+    console.log(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
 }
 
-function introductionWithLanguage(name = "Aki", language = "Ember.js"){
-    console.log('Hi, my name is ${name} and I am learning to program in ${language}.');
-}  
-
-function introductionWithLanguage(name = "Samip", language = "React"){
-    console.log('Hi, my name is ${name} and I am learning to program in ${language}.');
-}
-
-function introductionWithLanguageOptional(name = "Gracie", language = "JavaScript"){
-    console.log('Hi, my name is ${name} and I am learning to program in ${language}.');
-    console.log('Hi, my name is ${name} and I am learning to program in ${language}.');
-}
-
-function introductionWithLanguageOptional(name = "Gracie", language = "Python"){
-    console.log('Hi, my name is ${name} and I am learning to program in ${language}.');
-    console.log('Hi, my name is ${name} and I am learning to program in ${language}.');
-}
+introductionWithLanguageOptional("Gracie", "Python");
